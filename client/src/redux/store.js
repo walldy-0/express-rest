bf0dfdd4e1dafa83ab98c463562f4e02a6fa40ca
Waldemar Szymon <walldy@tlen.pl>
2023-07-03 13:14:02 +0200

    Change store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -15,7 +15,8 @@ const store = createStore(
   rootReducer,
   compose(
 		applyMiddleware(thunk),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f 
+		//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 	)
 );
 
